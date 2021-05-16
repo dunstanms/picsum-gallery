1f0eee31e8cfeca9f9522961c93881eca386a715
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-05-16 13:02:46 +0300

    add picture views

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from .models import Image, Category, Location
 # Create your views here.
+def welcome(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    return render(request, 'welcome.html', {"images": images, "locations": locations})
+
+def search_image(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',
+                      {'images': images, 'message': message, 'categories': categories,
+                       "locations": locations})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search.html', {"message": message})
+
+def get_image_by_location(request,location_name):
+    location_images = Image.filter_by_location(location_name)
+    locations = Location.objects.all()
+    location = location_name
+    return render(request, 'location.html', {"location_images": location_images, "location": location, "locations":locations})
